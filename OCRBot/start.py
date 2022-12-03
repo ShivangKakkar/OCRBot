a7d7ce54fea94898bfd0161da9f1130ab4763fdb
Snehashish Biswas <teamscenariox@gmail.com>
2022-12-03 22:38:39 +0530

    Pyrogram V2 + Code improvements 💫

--- OCRBot/start.py
+++ OCRBot/start.py
@@ -4,10 +4,9 @@ from pyrogram.types import InlineKeyboardMarkup
 
 
 # Start Message
-@Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
+@Client.on_message(filters.private & filters.incoming & filters.command("start"), group=4)
+async def start(bot, msg) -> str:
+	mention = bot.me.mention
 	await bot.send_message(
 		msg.chat.id,
 		Data.START.format(msg.from_user.mention, mention),
